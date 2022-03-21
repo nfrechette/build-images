ba6b884cf3658e93788c8fa0813b5d6da9a74372
Nicholas Frechette <zeno490@gmail.com>
2022-03-20 22:05:32 -0400

    fix: use a single image for xenial
    Will include all toolchains it supports

--- build_image_gcc-5.sh
+++ build_image_gcc-5.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker build --file Dockerfile-gcc \
+docker build --file Dockerfile-xenial \
     --platform linux/amd64 \
     --build-arg GCC_VERSION=5 \
-    --build-arg UBUNTU_VERSION=16.04 \
     --tag ghcr.io/nfrechette/toolchain-amd64-gcc-5:v1 .
