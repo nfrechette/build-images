ee4178131e1f73ca130b6768cb034feaa3d7b11f
Nicholas Frechette <zeno490@gmail.com>
2022-03-20 22:06:30 -0400

    chore: remove old files

--- build_image_gcc-4.8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --build-arg GCC_VERSION=4.8 --build-arg UBUNTU_VERSION=16.04 -t build-images:gcc-4.8 .
--- build_sjson-cpp_gcc-4.9.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-SRC=~/sjson-cpp
-exec docker run --rm -v $SRC:/project-src:Z build-images:gcc-4.9 python3 make.py -compiler gcc4.9 -build -unit_test -clean
