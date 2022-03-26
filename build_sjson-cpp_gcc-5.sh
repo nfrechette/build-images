259dc3e885568648ef0e7aa9289bd71c05a08524
Nicholas Frechette <zeno490@gmail.com>
2022-03-26 13:03:45 -0400

    fix: finalize xenial image and scripts

--- build_sjson-cpp_gcc-5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SRC=~/sjson-cpp
-exec docker run --rm \
-    -v $SRC:/project-src:Z \
-    ghcr.io/nfrechette/toolchain-amd64-xenial:v1 \
-    python3 make.py -compiler gcc5 -cpu x86 -build -unit_test -clean
