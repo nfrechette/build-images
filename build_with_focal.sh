7059780aab6b7a3917ad752eb5cbc3ddcfd62a87
Nicholas Frechette <zeno490@gmail.com>
2022-07-29 15:42:56 -0400

    chore: detect architecture and accept command line arguments

--- build_with_focal.sh
+++ build_with_focal.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-# Modify these to control what project builds and what architecture to use
-SRC=~/sjson-cpp
-ARCH=amd64
+# Source directory we'll map
+SRC=$1
+
+# Try and detect our architecture, we'll use it if we can
+sys_info=$(uname -a)
+if [[ $sys_info =~ x86_64 ]]; then
+    ARCH=amd64
+elif [[ $sys_info =~ arm64 ]] || [[ $sys_info =~ aarch64 ]]; then
+    ARCH=arm64
+else
+    # If we can't detect the architecture, use amd64
+    ARCH=amd64
+fi
+
+# If we specify a second argument, use it for our architecture
+if ! [ -z "$2" ]; then
+    ARCH=$2
+fi
 
 exec docker run -it --rm \
     -v $SRC:/project-src:Z \
