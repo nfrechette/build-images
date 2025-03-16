3b3208db9848aa0045003ddd9291d85cd55b1a8a
Nicholas Frechette <zeno490@gmail.com>
2025-03-16 10:42:46 -0400

    fix: update xenial launch script

--- build_with_xenial.sh
+++ build_with_xenial.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Modify these to control what project builds and what architecture to use
-SRC=~/sjson-cpp
-ARCH=amd64
+# Usage: .\build_with_focal.sh path/to/src/dir [arch]
+# First argument is source directory we'll map
+# Second argument is optional architecture to use, if omitted, we'll use the detected architecture
 
-exec docker run --rm \
+# Source directory we'll map
+SRC=$1
+
+# Try and detect our architecture, we'll use it if we can
+sys_info=$(uname -a)
+if [[ $sys_info =~ x86_64 ]]; then
+    ARCH=amd64
+elif [[ $sys_info =~ arm64 ]] || [[ $sys_info =~ aarch64 ]]; then
+    ARCH=arm64
+else
+    # If we can't detect the architecture, use amd64
+    ARCH=amd64
+fi
+
+# If we specify a second argument, use it for our architecture
+if ! [ -z "$2" ]; then
+    ARCH=$2
+fi
+
+exec docker run -it --rm \
     -v $SRC:/project-src:Z \
-    ghcr.io/nfrechette/toolchain-$ARCH-xenial:v1 \
-    "$@"
+    ghcr.io/nfrechette/toolchain-$ARCH-xenial:v1
